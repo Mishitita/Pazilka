69d8232fbd10ba82087210482a3b1f2063e91118
mishitita <147567659+Mishitita@users.noreply.github.com>
2024-04-28 19:25:48 -0600

    Add files via upload

---
+++ Pazilka/javita.js
@@ -0,0 +1,28 @@
+funtion loguear()
+{
+let user=document
+
+}
+
+let contador = 0;
+function sumar(){
+  contador++;
+  document.getElementById("contador").innerHTML = contador;
+  if(contador==100){
+    alert("CLICKEASTE 100 VECES. PRECIONA F5 PARA VOLVER A EMPEZAR")
+  }
+}
+document.getElementById("contador").addEventListener("click", sumar);
+
+
+<script>
+ function toggleMenu() {
+ var sidebar =
+document.getElementById("sidebar");
+ if (sidebar.style.display === "none") {
+ sidebar.style.display = "block";
+ } else {
+ sidebar.style.display = "none";
+ }
+}
+</script>
